155884f1a781028d24c9f248baa3db3fdea30a96
Robert Hearns <117030647+RobH95@users.noreply.github.com>
2023-03-07 23:10:47 +0000

    added baseurl to index.html and padding to project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ const Project = (props) => {
   const { title, deployedLink, repoLink, imageURL } = props;
 
   return (
-    <div className="col-sm">
+    <div className="col-sm" style={{padding: '16px'}}>
       <h2>{title}</h2>
       <p>Deployed version: <a href={deployedLink}>{deployedLink}</a></p>
       <p>GitHub repository: <a href={repoLink}>{repoLink}</a></p>
