655f6f86897db2da8c1fa29d3fa0a3fd3eb8d38b
Robert Hearns <117030647+RobH95@users.noreply.github.com>
2023-03-18 17:56:18 +0000

    Changed colour of  card and link text.

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,8 +6,8 @@ const Project = (props) => {
   return (
     <div className="col-sm" style={{padding: '16px'}}>
       <h2>{title}</h2>
-      <p>Deployed version: <a href={deployedLink}>{deployedLink}</a></p>
-      <p>GitHub repository: <a href={repoLink}>{repoLink}</a></p>
+      <p>Deployed version: <a href={deployedLink} target="_blank" rel="noopener noreferrer">{deployedLink}</a></p>
+      <p>GitHub repository: <a href={repoLink} target="_blank" rel="noopener noreferrer">{repoLink}</a></p>
       <img className="img-responsive fit-image" src={imageURL} alt={title} />
     </div>
   );
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -4,37 +4,37 @@ import { Container, Row, Col, Card } from 'react-bootstrap';
 const Projects = () => {
   const projects = [
     {
-      title: 'Weather Dashboard (8)',
+      title: 'Weather Dashboard',
       deployedLink: 'https://robh95.github.io/Challenge8-Weather-Dashboard/',
       repoLink: 'https://github.com/RobH95/Challenge8-Weather-Dashboard',
       imageURL: 'screenshots/challenge8.png'
     },
     {
-      title: 'Team Profile (12)',
+      title: 'Team Profile',
       deployedLink: 'https://robh95.github.io/Challenge12-TeamProfile/output/team.html',
       repoLink: 'https://github.com/RobH95/Challenge12-TeamProfile',
       imageURL: 'screenshots/challenge12.png'
     },
     {
-      title: 'README Generator (11)',
+      title: 'README Generator',
       deployedLink: 'https://robh95.github.io/challenge11-README-generator/',
       repoLink: 'https://github.com/RobH95/challenge11-README-generator',
       imageURL: 'screenshots/challenge11.png'
     },
     {
-      title: 'Calendar (7)',
+      title: 'Calendar',
       deployedLink: 'https://robh95.github.io/Challenge7-Calendar/',
       repoLink: 'https://github.com/RobH95/Challenge7-Calendar',
       imageURL: 'screenshots/challenge7.png'
     },
     {
-      title: 'Quiz (6)',
+      title: 'Quiz',
       deployedLink: 'https://robh95.github.io/Challenge6-Quiz/',
       repoLink: 'https://github.com/RobH95/Challenge6-Quiz',
       imageURL: 'screenshots/challenge6.png'
     },
     {
-      title: 'Password Generator (5)',
+      title: 'Password Generator',
       deployedLink: 'https://robh95.github.io/Challenge5-Password-gen/',
       repoLink: 'https://github.com/RobH95/Challenge5-Password-gen',
       imageURL: 'screenshots/challenge5.png'
@@ -57,7 +57,7 @@ const Projects = () => {
         <Row>
           {projects.map((project, index) => (
             <Col key={index} xs={12} sm={6} md={4} lg={3}>
-              <Card className="mb-4 cardEl">
+              <Card className="mb-4 card">
                 <Card.Img variant="top" src={project.imageURL} />
                 <Card.Body>
                   <Card.Title>{project.title}</Card.Title>
@@ -70,16 +70,6 @@ const Projects = () => {
           ))}
         </Row>
       </Container>
-
-      {/* <div className="card">
-        <div className="card-info">
-          <p className="title">John Doe</p>
-          <p className="subtitle">Web Dev</p>
-        </div>
-        <div className="card-bio">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div> */}
 
       <footer>
         <h1 className="Projects">&lt;/Projects&gt;</h1>
