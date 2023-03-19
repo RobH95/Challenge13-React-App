932d2a1f0794cfa8ce9b87729f7ce3ca1a17227e
Robert Hearns <117030647+RobH95@users.noreply.github.com>
2023-03-19 21:13:40 +0000

    Added logos for socials and linked them, changed the styling for logos. Added avatar and adjusted css.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Learn from './components/pages/Learn';
 import Projects from './components/pages/Projects';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheckSquare, faCoffee);
+
 
 
 function App() {
@@ -27,7 +32,30 @@ function App() {
           <Route path="/challenge13-react-app/projects" element={<Projects />} />
         </Routes>
       </div>
+       
+      <div className="button-container">
+        <div className="glass-btn blue-btn">
+          <img src="https://i.postimg.cc/DwbWDQTx/facebook.png" alt="facebook" style={{width: '5.5em'}} />
+        </div>
+
+        <div className="glass-btn red-btn">
+          <img src="https://i.postimg.cc/LstJ4Hhf/youtube.png" alt="youtube" style={{width: '6em'}} />
+        </div>
+
+        <div className="glass-btn amber-btn">
+          <a href="https://www.linkedin.com/in/robert-hearns-74015377/">
+            <img src="images/linkedin-logo.png" alt="linkedin"  className="social-icon" style={{width: '2.5em'}} />
+          </a>
+        </div>
+
+        <div className="glass-btn amber-btn">
+          <a href="https://github.com/RobH95">
+            <img src="images/github-logo-face.png" className="social-icon" alt="github" style={{width: '3em'}} />
+          </a>
+        </div>
+      </div>
     </Router>
+    
   );
 }
 
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-
 
 function Contact(props) {
   return (
     <div>
-      <h1 className="Contact">&lt;Contact&gt;</h1>
-      <p>
-        </p>
-        <p></p>
-
-      <footer>
-        <h1 className="Contact">&lt;/Contact&gt;</h1>
-      </footer>
-
+     
     </div>
   );
 }
